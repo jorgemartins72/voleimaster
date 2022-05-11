075ef0d7ba06a3e92bfd0421597d0451cd36df67
Jorge Martins <jorgemartins72@gmail.com>
2022-05-11 05:12:39 -0300

    correção no breadcrumb

--- components/breadcrumb.vue
+++ components/breadcrumb.vue
@@ -24,8 +24,8 @@
 						<svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="w-4 h-5 text-gray-300">
 							<path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
 						</svg>
-						<div class="ml-2 text-sm font-bold text-gray-300">
-							{{this.subItemExtra.nome}}
+						<div class="ml-2 text-sm font-bold text-gray-300" v-if="subItemExtra">
+							{{subItemExtra.nome}}
 						</div>
 					</div>
 				</li>
