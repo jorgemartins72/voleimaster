fe16fbe7185cbf7783668428590cb997b3814249
Jorge Martins <jorgemartins72@gmail.com>
2022-05-24 19:14:27 -0300

    formulário faleconosco pronto

--- pages/faleconosco.vue
+++ pages/faleconosco.vue
@@ -16,8 +16,8 @@
 					<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
 						<span class="block xl:inline">Fale conosco</span>
 					</h1>
-					<p class="mt-3 mb-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-						Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+					<p class="mt-3 mb-6 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+						Mande sua mensagem preenchendo o formulário abaixo com sua dúvida, sugestão, crítica ou reclamação.
 					</p>
 
 
