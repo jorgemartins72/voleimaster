0c42feb2cd5d83d007a282a8a0f838e2a3d74223
Jorge Martins <jorgemartins72@gmail.com>
2022-06-21 14:02:24 -0300

    página transparência pronta

---
+++ pages/quemsomos/pagecomps/transparencia.vue
@@ -0,0 +1,67 @@
+<template>
+	<div>
+		<h1 class="font-inter-800 font-black uppercase text-2xl text-gray-600 mb-4">
+			TRANSPARÊNCIA
+		</h1>
+		<div class="md:px-8 mb-8">
+			<div class="space-y-4 grid grid-cols-1">
+				<p class="text-xl leading-7 font-black text-gray-600">
+					"Texto de destaque, texto de destaque, texto de destaque, texto de destaque, texto de destaque, texto de destaque, texto de destaque, texto de destaque, texto de destaque, texto de destaque"
+				</p>
+				<p class="text-base text-gray-600">
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci rerum alias odit delectus animi consequatur dolore quasi, est minima excepturi dignissimos reprehenderit sequi quam ipsum laboriosam dolorem saepe, blanditiis eos?
+				</p>
+			</div>
+		</div>
+		
+		<div class="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-6 mb-12">
+			<div class="font-inter-800 font-black uppercase text-xl text-gray-600 mb-4">
+				Integrantes
+			</div>
+
+			<div class="md:col-span-4 lg:col-span-5 grid md:grid-cols-2 lg:grid-cols-3 gap-4 md:pt-2">
+
+				<div class="mx-auto md:mx-0 w-9/12 md:w-full">
+					<div 
+						class="w-full h-60 md:h-56 bg-cover rounded-lg mb-1"
+						style="background-image: url('/quemsomos/transparencia/integrantes/valter.jpg')"
+					/>
+					<div class="text-sm">VALTER DE PAULA GAUDIO</div>
+					<div class="font-black text-indigo-900 mb-1">Conselheiro Fiscal</div>
+
+				</div>
+
+				<div class="mx-auto md:mx-0 w-9/12 md:w-full">
+					<div 
+						class="w-full h-60 md:h-56 bg-cover rounded-lg mb-1"
+						style="background-image: url('/quemsomos/transparencia/integrantes/jose.jpg')"
+					/>
+					<div class="text-sm">JOSÉ PERILLIER</div>
+					<div class="font-black text-indigo-900 mb-1">Conselheiro Fiscal</div>
+
+				</div>
+
+				<div class="mx-auto md:mx-0 w-9/12 md:w-full">
+					<div 
+						class="w-full h-60 md:h-56 bg-cover rounded-lg mb-1"
+						style="background-image: url('/quemsomos/transparencia/integrantes/eduardo.jpg')"
+					/>
+					<div class="text-sm">EDUARDO NASCIMENTO</div>
+					<div class="font-black text-indigo-900 mb-1">Conselheiro Fiscal</div>
+
+				</div>
+
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
